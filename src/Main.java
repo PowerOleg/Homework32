ae13b425ed7f28c43d69f2f4ff82e629040bae73
PowerOleg <geraskin.oleg@mail.ru>
2023-01-21 19:50:10 +0500

    Task2 is done

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bin = new BinOps();
+        System.out.println(bin.sum("1010011", "110001"));
+        System.out.println(bin.mult("11", "1"));
+    }
+}
